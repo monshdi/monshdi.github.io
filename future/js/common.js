5020f46bcbe39f1e8ba6b201ccb4b23ee21b8045
monshdi <dimamonch1@gmail.com>
2018-10-29 21:53:05 +0700

    commit

--- future/js/common.js
+++ future/js/common.js
@@ -1,27 +1,31 @@
+
 $(function() {
 
+	$('.portfolio_block').on('mousewheel', function(event) {
+});
+
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 0) {
-			$('#item1').addClass('active')
+		if ($(this).scrollTop() >= $('.portfolio_block').offset().top + 0) {
+			$('#item1').addClass('active');
 		}
 		else{
 			$('#item1').removeClass('active')
 		}
-		if ($(this).scrollTop() >= 120) {
+		if ($(this).scrollTop() >= $('.portfolio_block').offset().top + 110) {
 			$('#item1').removeClass('active')
 			$('#item2').addClass('active')
 		}
 		else{
 			$('#item2').removeClass('active')
 		}
-		if ($(this).scrollTop() >= 220) {
+		if ($(this).scrollTop() >= $('.portfolio_block').offset().top + 210) {
 			$('#item2').removeClass('active')
 			$('#item3').addClass('active')
 		}
 		else{
 			$('#item3').removeClass('active')
 		}
-		if ($(this).scrollTop() >= 320) {
+		if ($(this).scrollTop() >= $('.portfolio_block').offset().top + 310) {
 			$('#item3').removeClass('active')
 			$('#item4').addClass('active')
 		}
