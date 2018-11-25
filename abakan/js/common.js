d9c7c43e7f334c567bff8712d37296d6fb35ab63
monshdi <dimamonch1@gmail.com>
2018-11-25 19:05:16 +0700

    Commit

--- abakan/js/common.js
+++ abakan/js/common.js
@@ -1,8 +1,31 @@
-$(function() {
+window.addEventListener('DOMContentLoaded', function(){
+	// Аккордеон
+	let acc = document.getElementsByClassName('accordeon'),
+		accContent = document.querySelectorAll('.tech_wrap_block_item_descr'),
+		arrow = document.getElementsByClassName('arrow_button');
+		
 
-	// Custom JS
-	$('.service_btn').click(function(event){
-		$('.service_content_hide').toggleClass('active_content');
-	});
+	for (let i = 0; i < acc.length; i++) {
+		acc[i].addEventListener('click', function(){
+			accContent[i].classList.toggle('active_descr');
+			arrow[i].classList.toggle('active_arrow');
+		})
+	};
+	// Раскрытие
+	let showBtn = document.getElementsByClassName('service_btn'),
+	 	hideContent = document.getElementsByClassName('service_content_hide');
+
+	 	for (let i = 0; i < showBtn.length; i++) {
+	 		showBtn[i].addEventListener('click', function(){
+	 			hideContent[i].classList.add('active_content');
+	 			setTimeout(function(){
+	 				hideContent[i].classList.remove('active_content');
+	 			}, 6000);
+		 	});
+		 }
+
+
+
+
 
 });
