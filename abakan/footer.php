32ed7f5e3b40d40f22ed6f2660b22bf665f8075c
monshdi <dimamonch1@gmail.com>
2018-11-26 19:32:18 +0700

    Commit

---
+++ abakan/footer.php
@@ -0,0 +1,15 @@
+
+<footer class="main_foot">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 left_foot align-self-center">2010 - 2018 &copy; Мастерская домашнего уюта "ХакасСтрой"<p>Отделочные работы в Абакане</p></div>
+			<div class="col-lg-4 align-self-center center_foot"><strong>8-888-888-88-88</strong></div>
+			<div class="col-lg-4 right_foot align-self-center"><p>Сайт создан при помощи студии <a href="#">WebDevAtellier</a></p></div>
+		</div>
+	</div>
+</footer>
+
+<script src="js/scripts.min.js"></script>
+
+</body>
+</html>
