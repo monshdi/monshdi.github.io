0df86e41f15477592dffdb4b28b94249c70c4f3b
monshdi <dimamonch1@gmail.com>
2019-02-03 22:50:57 +0700

    Верстка главной страницы[C

---
+++ watelltodo/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	$('.btn-wh').click(function() {
+		$('.login_wrap').removeClass('login_vis');
+	});
+	$('.btn-close').click(function() {
+		$('.login_wrap').addClass('login_vis');
+	});
+
+});
